9e211325f74f4dfd91110ec28621db684d7748f0
Kingswill Wariboko <kingswillwariboko@gmail.com>
2023-01-12 15:46:04 +0100

    another changes

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,4 +1,5 @@
-import React, {useState, useEffect} from "react"
+import React, { useEffect, useState } from 'react'
+import { createClient } from "contentful"
 import next from "../../assets/icon-next.svg"
 import previous from "../../assets/icon-previous.svg"
 
@@ -6,25 +7,29 @@ import previous from "../../assets/icon-previous.svg"
 
 import "./carousel.scss"
 
-const data = [
-    {
-        image: 'https://user-images.githubusercontent.com/54893238/211214114-47f4df9f-c533-4d21-b920-8b44e4761b6f.jpeg'
-    },
-    {
-        image: 'https://user-images.githubusercontent.com/54893238/211214122-3bd721d5-8ba7-4de1-a026-860884afc4b8.jpeg'
-    },
-    {
-        image: 'https://user-images.githubusercontent.com/54893238/211214197-fb3b4ff7-f0fb-4082-b588-b3c1962f7ca3.jpeg'
-    }
-]
-
 const Carousel = ()=> {
+    const [images, setImages] = useState([])
+  const client = createClient({ space: "audrfmrh2x7a", accessToken: "Ol36WYE4bG73TURBza9PYrYYdx2hg4u2sjiBwC9X46g"})
+
+  useEffect(() => {
+    const getAllEntries = async () => {
+      try {
+        await client.getAssets().then((entries) => {
+          console.log(entries)
+          setImages(entries)
+        })
+      } catch (error) {
+        console.log(`Error fetching authors ${error}`);
+      }
+    };
+    getAllEntries()
+  }, [])
     
 
     const[currentIndex, setCurrentIndex] = useState(0)
 
     const carouselScroll = () =>{
-        if(currentIndex === data.length -1){
+        if(currentIndex === images.length -1){
             return setCurrentIndex(0)
         }else{
             setCurrentIndex(currentIndex + 1)
@@ -32,7 +37,7 @@ const Carousel = ()=> {
     }
 
     const nextMove = ()=>{
-        if(currentIndex === data.length -1){
+        if(currentIndex === images.length -1){
             return setCurrentIndex(0)
         }else{
             setCurrentIndex(currentIndex + 1)
@@ -40,7 +45,7 @@ const Carousel = ()=> {
     }
 
     const previousMove = () =>{
-        if(currentIndex === data.length -1){
+        if(currentIndex === images.length -1){
             return setCurrentIndex(currentIndex - 1)
         }else{
             setCurrentIndex(currentIndex - 1)
@@ -57,11 +62,11 @@ const Carousel = ()=> {
     return(
         <main className="main-main">
             <div className="carousel-container">
-                {data.map((item, index)=> {
-                    return <div key={index} className="carousel-item"
-                    style={{transform: `translate(-${currentIndex * 100}%)`}}><img src={item.image} alt=""/></div>
+            {images?.items?.map((image) => (
+                     <div key={image.fields.title} className="carousel-item"
+                    style={{transform: `translate(-${currentIndex * 100}%)`}}><img src={image.fields.file.url} alt=""/></div>
                     
-                })}
+                ))}
 
                 <button  onClick={nextMove} className="next">
                     <img src={next} alt="next" />
@@ -74,19 +79,19 @@ const Carousel = ()=> {
 
             
             <div className="carousel-container-desktop">
-                    {data.map((item, index)=> {
-                        return <div key={index} className="carousel-item"
-                        style={{transform: `translate(-${currentIndex * 100}%)`}}><img src={item.image} alt=""/></div>
+            {images?.items?.map((image) => (
+                     <div key={image.fields.title} className="carousel-item"
+                        style={{transform: `translate(-${currentIndex * 100}%)`}}><img src={image.fields.file.url} alt=""/></div>
                         
-                    })}        
+                        ))}      
             </div>    
 
             <div className="carousel-menu">
-                           {data.map((item, index)=>{
-                            return <div onClick={()=>  setCurrentIndex(index)} className={currentIndex === index ? "main": 'round'} >
-                                    <img src={item.image} alt=""/>
+            {images?.items?.slice(0,3).map((image, index) => (
+                           <div key={image.fields.title} onClick={()=>  setCurrentIndex(index)} className={currentIndex === index ? "main": 'round'} >
+                                    <img src={image.fields.file.url} alt=""/>
                                 </div>
-                           })}
+                           ))}  
             </div>  
 
             
--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useEffect, useState } from 'react'
+import { createClient } from "contentful"
 import plus from "../../ecommerce-product-page-main/images/icon-plus.svg"
 import close from "../../ecommerce-product-page-main/images/icon-minus.svg"
 import cart from "../../ecommerce-product-page-main/images/icon-cart.svg"
@@ -6,15 +7,33 @@ import cart from "../../ecommerce-product-page-main/images/icon-cart.svg"
 import "./Cart.scss"
 
 const Cart = ()=>{
+
+  const [info, setInfo] = useState([])
+  const client = createClient({ space: "audrfmrh2x7a", accessToken: "Ol36WYE4bG73TURBza9PYrYYdx2hg4u2sjiBwC9X46g"})
+
+  useEffect(() => {
+    const getAllEntries = async () => {
+      try {
+        await client.getEntries().then((entries) => {
+          console.log(entries)
+          setInfo(entries)
+        })
+      } catch (error) {
+        console.log(`Error fetching authors ${error}`);
+      }
+    };
+    getAllEntries()
+  }, [])
+
+
     return(
+        <>
+         {info?.items?.map((post) => (
         <div className="cart">
             <div className="cart__one">
-                <h4>Car pump</h4>
-                <h1>Fall Limited version</h1>
-                <p>Add a screenshot of your solution. The easiest way 
-                    to do this is to use Firefox to view your project, right-click
-                     the page and select 
-                    "Take a Screenshot". You can choose either a full-height screensho</p>
+                <h4>{post.fields.firstTitle}</h4>
+                <h1>{post.fields.secondTitle}</h1>
+                <p>{post.fields.description}</p>
             </div>
             <div className="cart__two">
                 <div className="cart__two-one">
@@ -27,7 +46,8 @@ const Cart = ()=>{
                     <button className="add-btn">Buy now</button>
                 </div>
             </div>
-        </div>
+        </div>))}
+        </>
     )
 }
 
